516b29803cbcd799d6ccf457bb557ad0cf2275d4
cesiliaangita <cesiliaanggita26@ymail.com>
2013-09-20 11:05:30 +0700

    sesi3

---
+++ person.java
@@ -0,0 +1,11 @@
+class Person
+{
+	public static void main(String[] abc)
+	{
+	int x=10;
+	char y;
+	y='A';
+		System.out.println("menampilkan variable x=" +x);
+		System.out.println("menampilkan variable y=" +y);
+	}
+}
